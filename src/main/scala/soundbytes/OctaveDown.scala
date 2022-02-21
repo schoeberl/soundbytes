277c513671f271a402044423e5ed564f7db7765e
Clemens Pircher <clemens.lukas@gmx.at>
2022-02-22 00:50:05 +0100

    Implement Lookahead in OctaveDown

--- src/main/scala/soundbytes/OctaveDown.scala
+++ src/main/scala/soundbytes/OctaveDown.scala
@@ -6,36 +6,48 @@ import chisel3.util._
 
 /**
  * Experimental octave down effect based on an indirect divider circuit.
- * Does not work very well at the moment.
+ * Somewhat working version.
  *
  * Potential improvement:
- * 1) Lowpass filtering (i.e. counter) on the regControl flipping condition to track the peaks more accurately.
- *    This would require having multiple samples on the fly at the same time until a decision for regControl can be made.
+ * 1) Tweaking of regControl flipping logic and good parameter set (based on real-world samples) is necessary
  * 2) Filtering of the output signal should help to eliminate artifacts.
  */
-class OctaveDown(dataWidth: Int = 16) extends Module {
+class OctaveDown(dataWidth: Int = 16, bufferLength: Int = 16) extends Module {
   val io = IO(new Bundle {
     val in = Flipped(new DecoupledIO(SInt(dataWidth.W)))
     val out = new DecoupledIO(SInt(dataWidth.W))
   })
 
+  val maxSignal = (1 << (dataWidth - 1)) - 1
+  val maxIndex = bufferLength - 1
+  val indexBits = log2Up(bufferLength)
+  
+  val margin = 20
+
   // State Variables.
-  val idle :: hasValue :: Nil = Enum(2)
-  val regState = RegInit(idle)
+  val sampleCount = RegInit(0.U(indexBits.W))
 
-  val regInVal = RegInit(0.S(dataWidth.W))
   val regLowPeak = RegInit(0.S(dataWidth.W))
+  val regLowPeakIndex = RegInit(0.U(indexBits.W))
 
-  val regRising = RegInit(false.B)
+  val regOffset = RegInit(0.S(dataWidth.W))
+
+  val regRising = RegInit(0.U(indexBits.W))
   val regControl = RegInit(false.B)
+  val regToggled = RegInit(false.B)
 
   // Input.
   val inVal = io.in.bits
   
-  // Output.
-  val valOffset = regInVal -& regLowPeak
-  val valScaled = valOffset >> 1
+  // Buffer.
+  val counter = Counter(bufferLength)
+  val buffer = SyncReadMem(bufferLength, SInt(dataWidth.W), SyncReadMem.ReadFirst)
+  val bufferSample = buffer(counter.value)
   
+  // Output.
+  val valOffset = bufferSample -& regOffset
+  val valScaled = valOffset >> 1  
+
   when (regControl) {
     io.out.bits := -valScaled
   }.otherwise {
@@ -43,30 +55,46 @@ class OctaveDown(dataWidth: Int = 16) extends Module {
   }
   
   // FSM.
-  io.in.ready := regState === idle
-  io.out.valid := regState === hasValue
-  switch (regState) {
-    is (idle) {
-      when(io.in.valid) {
-        when(!regRising & inVal(dataWidth - 1) & inVal > regInVal) {
-          regRising := true.B
-          regLowPeak := regInVal
-          regControl := !regControl
-        }
-        
-        when(regRising & !inVal(dataWidth - 1) & inVal < regInVal) {
-          regRising := false.B
-        }
-        
-        regInVal := io.in.bits
-        
-        regState := hasValue
+  io.in.ready := false.B
+  io.out.valid := false.B
+  
+  when(io.in.valid & sampleCount < maxIndex.U) {    // write to module when not full
+    when(inVal > margin.S & regToggled) {                // clearing for next peak detection
+      when(regRising < maxIndex.U) {
+        regRising := regRising + 1.U
+      }.otherwise {
+        regToggled := false.B
+        regRising := 0.U
+        regLowPeak := inVal
       }
     }
-    is (hasValue) {
-      when(io.out.ready) {
-        regState := idle
+    
+    when(regRising < maxIndex.U & !regToggled) {    // peak detection
+      when(inVal < margin.S & inVal >= regLowPeak) {     // advance peak detection
+        regRising := regRising + 1.U
+      }.otherwise {                                 // reset peak detection
+        regRising := 0.U
+        regLowPeak := inVal
+        regLowPeakIndex := counter.value
       }
     }
+    
+    bufferSample := inVal
+    sampleCount := sampleCount + 1.U
+    counter.inc()
+    io.in.ready := true.B
   }
+
+  when(io.out.ready & sampleCount === maxIndex.U) {
+    when(counter.value === regLowPeakIndex & regRising === maxIndex.U & !regToggled) {
+      regOffset := regLowPeak
+      regControl := !regControl
+      regRising := 0.U
+      regToggled := true.B
+    }
+    
+    sampleCount := sampleCount - 1.U
+    io.out.valid := true.B
+  }
+
 }
